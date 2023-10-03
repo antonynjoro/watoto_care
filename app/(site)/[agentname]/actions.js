d56637aee761ea9405e2fac1571ddc5bb17afc00
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-10-03 11:59:40 -0600

    Implemented a shift in Data request to use server

---
+++ app/(site)/[agentname]/actions.js
@@ -0,0 +1,25 @@
+"use server"
+
+import prisma from "../../libs/prismadb";
+import { NextResponse } from 'next/server';
+
+export async function fetchAgentData(agentname) {
+    
+    // Fetch the agent data from the database
+    const agentData = await prisma.user.findUnique({
+        where: {
+            username: agentname
+        }
+    });
+
+    // If the agent doesn't exist, return a 404
+    if (!agentData) {
+        return new NextResponse('Agent not found', {status: 404});
+    }
+
+    // Return the agent data
+    return NextResponse.json(agentData);
+
+
+    
+}
