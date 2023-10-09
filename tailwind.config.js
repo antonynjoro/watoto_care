d675e615375f2366c1308fbc09563619b6756d68
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-10-08 18:08:22 -0600

    Constructed the Daycare page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        "sans": ["Open Sans", "sans-serif"],
+      }
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
